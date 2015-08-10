1890ffbaaa81dfa61e7fecc50d1ee86c1a13a50a
anhdtuet1995 <anhdtuet1995@gmail.com>
2015-08-10 16:01:56 +0700

    finish join conga

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class EnemyController : MonoBehaviour {
 	public float speed = -1;
 	private Transform spawnPoint;
+	private Camera myCamera;
 	// Use this for initialization
 	void Start () {
+		myCamera = Camera.main;
 		GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
 		spawnPoint = GameObject.Find("SpawnPoint").transform;
 	}
@@ -17,9 +19,9 @@ public class EnemyController : MonoBehaviour {
 
 	void OnBecameInvisible()
 	{	
-		if (Camera.main == null)
+		if (myCamera == null)
 			return;
-		float yMax = Camera.main.orthographicSize - 0.5f;
+		float yMax = myCamera.orthographicSize - 0.5f;
 		transform.position = new Vector3( spawnPoint.position.x, 
 		                                 Random.Range(-yMax, yMax), 
 		                                 transform.position.z );
